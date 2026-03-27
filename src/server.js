5437814acbf7137c3aca7df3c8f2553d96472628
Jadson <jadsonzap@gmail.com>
2026-03-27 17:46:37 -0300

    Alteração de link do cors

--- src/server.js
+++ src/server.js
@@ -10,7 +10,7 @@ const port = process.env.PORT || 5001;
 
 app.use(express.json());
 app.use(cors({
-    origin: "https://helogin-77x5u3gzk-jadson-h7s-projects.vercel.app/"
+    origin: "https://helogin.vercel.app/"
 }));
 
 app.use('/api', router);
