11f0d0ff85208cb7ce88373e72c9ab51a573001e
Jadson <jadsonzap@gmail.com>
2026-03-27 20:19:02 -0300

    ...

--- src/server.js
+++ src/server.js
@@ -10,7 +10,7 @@ const port = process.env.PORT || 5001;
 
 app.use(express.json());
 app.use(cors({
-    origin: "https://helogin.vercel.app/api/"
+    origin: "https://helogin.vercel.app/"
 }));
 
 app.use('/api', router);
